310453f52b02b328d74d01d68f59261048d9a0a3
zdahlgren <zdahlgren@comcast.net>
2024-05-18 10:47:19 -0700

    Sat cleanup

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def my_form():
     return render_template('refinance.html',
                            pp_value = default_config["purchase_price"], 
                            ap_value = default_config["appraised_value"], 
+                           equity_value = round(int(default_config["purchase_price"]) * 0.25),
                            rent_value = default_config["rent"],
                            taxes_value = default_config["taxes"],
                            insurance_value = default_config["insurance"],
@@ -27,17 +28,36 @@ def my_form():
 def my_form_post():
     purchase_price = int(request.form['purchase_price'])
     appraised_value = int(request.form['appraised_value'])
+    equity = int(request.form['equity'])
+
     target_rent = int(request.form['target_rent'])
-    refi = Refinance(purchase_price=purchase_price, appraised_value=appraised_value, rent=target_rent)
+    refi = Refinance(purchase_price=purchase_price, appraised_value=appraised_value, equity=equity, rent=target_rent)
     refi.set_insurance(int(request.form['insurance']))
     refi.set_prop_taxes(int(request.form['taxes']))
     refi.set_interest_rate(float(request.form['interest_rate']))
     
     refi.set_monthly_payment(amoritization=int(default_config["amoritization"]))
     actual_dscr = refi.calculate_dscr()
+
+    workable_scenarios = ""
+    happy_net = ""
+    happy_rent = "" 
+    happy_monthly_payment = ""
+    happy_ltv = ""
+    if not refi.is_feasible(actual_dscr):
+        happy_tup = refi.happy_path_scenarios(amoritization=int(default_config["amoritization"]))
+        workable_scenarios = f"To make numbers work at {refi.target_dscr} DSCR, you would need one of the following:" 
+        happy_net = f"New Net: ${round(happy_tup[0], 2)}" 
+        happy_rent = f"Rent Increase: ${round(happy_tup[1], 2)}" 
+        happy_monthly_payment = f"Happy Monthly Payment: ${round(happy_tup[2], 2)}"
+        happy_ltv = f"LTV: {round(happy_tup[3], 2)}%"
+    else:
+        cashout = (refi.appraised_value - refi.purchase_price) - ( (refi.appraised_value * (1-refi.ltv) - refi.equity)) - (refi.appraised_value * refi.refi_fee_rate)
+
     return render_template("refinance.html",  
                            pp_value = purchase_price, 
-                           ap_value = appraised_value, 
+                           ap_value = appraised_value,
+                           equity_value = equity,  
                            rent_value = target_rent,
                            taxes_value = refi.prop_taxes,
                            insurance_value = refi.insurance,
@@ -45,7 +65,14 @@ def my_form_post():
                            MONTHLY_PAYMENT = f"Monthly Payment: ${round(refi.monthly_payment, 2)}",
                            CASH_ON_CASH_RETURNS = f"Cash on Cash: {round(refi.calculate_cash_on_cash(), 2)}%",
                            DSCR_RATIO = f"DSCR: {round(actual_dscr, 2)}", 
-                           FEASIBILITY = f" Feasible?: {refi.determine_feasibility(actual_dscr=actual_dscr)}")
+                           FEASIBILITY = f"Feasible?: {refi.is_feasible(actual_dscr=actual_dscr)}",
+                           CASHOUT = f"Cashout: ${round(cashout, 2)}",
+                           WORKABLE_SCENARIOS = workable_scenarios,
+                           HAPPY_NET = happy_net,
+                           HAPPY_RENT_INCREASE = happy_rent,
+                           HAPPY_MONTHLY_PAYMENT = happy_monthly_payment,
+                           HAPPY_LTV = happy_ltv
+                           )
 
 @app.route("/", methods=['GET', 'POST'])
 def func():
--- refinance.py
+++ refinance.py
@@ -10,7 +10,7 @@ class Refinance:
     target_dscr = 1.2
     refi_fee_rate = 0.03
 
-    ltv = 0.8
+    ltv = 0.80
     monthly_payment = 0
 
     gross = 0
@@ -18,8 +18,9 @@ class Refinance:
     expenses = 0
 
 
-    def __init__(self, purchase_price, appraised_value, rent=0):
+    def __init__(self, purchase_price, appraised_value, equity, rent=0):
         self.purchase_price = purchase_price
+        self.equity = equity
         self.appraised_value = appraised_value
         self.rent = rent
     
@@ -45,6 +46,7 @@ class Refinance:
         self.monthly_payment = -npf.pmt((self.interest_rate / 100)/12, amoritization *12, self.appraised_value * self.ltv)
         self.monthly_payment += (self.insurance/12)
         self.monthly_payment += (self.prop_taxes/12)
+
     def set_target_dscr(self, target_dscr):
         self.target_dscr = target_dscr
     
@@ -60,14 +62,30 @@ class Refinance:
             return actual_dscr
         except:
             return 0
+        
+    def happy_path_scenarios(self, amoritization):
+        self.set_gross_net_and_expenses()
+        happy_net = (12 * self.monthly_payment) * self.target_dscr
+        happy_rent_increase = (happy_net - self.net) / 12
+        happy_monthly_payment = (self.net / self.target_dscr ) /12
+        happy_ltv = self.find_happy_ltv(self.ltv, amoritization, happy_monthly_payment)
+        return(happy_net, happy_rent_increase, happy_monthly_payment, happy_ltv)
+    
+    def find_happy_ltv(self, ltv, amoritization, happy_monthly_payment):
+        value = -npf.pmt((self.interest_rate / 100)/12, amoritization *12, self.appraised_value * ltv) 
+        value += (self.insurance/12) 
+        value += (self.prop_taxes/12)
+        if happy_monthly_payment < value:
+            happy_ltv = self.find_happy_ltv((ltv - 0.05), amoritization+10, happy_monthly_payment)
+        else:
+            happy_ltv = ltv
+        return happy_ltv
     
     def calculate_cash_on_cash(self):
         income = self.net - self.monthly_payment*12
         invested = (self.appraised_value * (1.0- self.ltv)) + (self.appraised_value*self.refi_fee_rate)
         return (income / invested)*100
 
-    def determine_feasibility(self, actual_dscr):
-        if actual_dscr >= self.target_dscr:
-            return True
-        return False
+    def is_feasible(self, actual_dscr):
+        return actual_dscr >= self.target_dscr
         
